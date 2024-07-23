a5e62c04ccf98b94cd6a6360d3ae299b121ece67
jmatondo <j_matondo@hotmail.com>
2024-07-23 21:46:45 +0200

    xxx

--- components/nav/header.tsx
+++ components/nav/header.tsx
@@ -1,14 +1,26 @@
 import Link from "next/link";
 import React from "react";
-import { MdCamera, MdGroup } from "react-icons/md";
+import { MdCamera, MdGroup, MdLogin, MdLogout } from "react-icons/md";
 
 const Header = () => {
   return (
-    <div className="bg-gray-200 flex items-center py-4 p-2">
-      <MdCamera size={25} />
-      <Link href="/home" className="font-medium">
-        Team Photo
-      </Link>
+    <div className="bg-gray-200 flex justify-between items-center p-4">
+      <div className="flex items-center gap-2">
+        <MdCamera size={30} className="text-sky-600" />
+        <Link href="/home" className="font-medium">
+          Team Photo
+        </Link>
+      </div>
+      <div>
+        {"1" == "1" ? (
+          <MdLogin size={30} className="text-green-600" />
+        ) : (
+          <p className="flex ju gap-2 items-center">
+            <span className="">Etia</span>
+            <MdLogout size={30} className="text-red-600" />
+          </p>
+        )}
+      </div>
     </div>
   );
 };
